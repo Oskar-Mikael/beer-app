1b8e76b1835c0ec3996eb96caee0743e03afd538
Oskar-Mikael <oskar.m.bostrom@gmail.com>
2021-09-04 22:09:55 +0200

    Auth, models, controllers and migrations scaffolding

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function list()
+    {
+        return $this->hasMany(BeerList::class);
+    }
 }
---
+++ database/migrations/2021_09_04_192641_create_beers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->string('beer');
+            $table->integer('rating');
+            $table->string('country');
+            $table->string('type');
+            $table->string('review')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
