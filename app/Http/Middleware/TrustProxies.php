efd7b9cf1b0e82466107f881ed6bac1a1e88e40b
Oskar-Mikael <oskar.m.bostrom@gmail.com>
2025-08-09 20:26:39 +0000

    Trust proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '192.168.0.99';
 
     /**
      * The headers that should be used to detect proxies.
